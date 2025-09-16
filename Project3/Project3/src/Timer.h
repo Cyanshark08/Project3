22d91f613df0c84dd80870770c6b49689db7e1ee
Landon Mendoza <landonmendoza@outlook.com>
2025-09-15 18:56:54 -0700

    Documentation

--- Project3/Project3/src/Timer.h
+++ Project3/Project3/src/Timer.h
@@ -7,9 +7,26 @@ public:
 	typedef std::chrono::time_point<std::chrono::high_resolution_clock> time_point;
 
 	Timer();
+
+	/*
+	* Precondition: None
+	* 
+	* Postcondition: Reinitializes m_InitialTime to the current time
+	*/
 	time_point Reset();
 
+	/*
+	* Precondition: Timer must be initialized
+	*
+	* Postcondition: Returns initial time_point
+	*/
 	time_point GetInitialTime() const;
+
+	/*
+	* Precondition: Timer must be initialized
+	*
+	* Postcondition: Returns elapsed time since initial time
+	*/
 	float GetElapsedTime() const;
 	
 private:
